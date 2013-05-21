7267a966a3320abce186d0746e7706a1a7f0ec38
roshow <rolando@roshow.net>
2013-05-21 01:49:19 -0400

    initial files

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div style='width:100%;border: 1px solid black; margin-top:10px; display:block'></div>
+<p>...and we're done </p>
+</body>
+</head>
+</html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>roshow's theme</title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	</head>
+	<body>
+		<a href='index.php'><div style='width:100%;position:fixed;top:0;left:0;display:block;'><img src='wp-content/themes/roshow-wp/images/header.png' id='header_img'/></div></a>
