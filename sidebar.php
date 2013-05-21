26656eec3f391a35dfc19d7d4c7ef0db7514d42d
roshow <rolando@roshow.net>
2013-05-21 03:33:09 -0400

    more styling for sidebar; added blogroll back in; new header image sized down to 100px tall

--- sidebar.php
+++ sidebar.php
@@ -1,55 +1,44 @@
 <div id="sidebar">
-<div id="sidebar-top"></div>
-
-<div id="sidebar-content">
-
-			<div id="subcolumn">
-				
-
-	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : ?>		
-
-
-		
-		<div class="widget">			
-				<h2 class="hl">Calendar</h2>
-				<div align="center">
-					<?php get_calendar(1); ?> 
-				</div>
-		</div>
-		
-
-		
-		<div class="widget">			
-				<h2 class="hl">Categories</h2>
-				<ul>
-						<?php wp_list_cats('sort_column=name&optioncount=1'); ?>
-				</ul>
-		</div>
-		
-		<div class="widget">			
-				<h2 class="hl">Archives</h2>
-				<ul>
-						<?php wp_get_archives('type=monthly&show_post_count=1'); ?>
-				</ul>
-		</div>
-		
-		<?php endif; ?>
-
-
-		<div id="search">
-					 <form id="searchform" method="get" action="<?php bloginfo('home'); ?>/">
-						<div>
-							<input type="text" name="s" id="s" value="" size="35" class="form-input" />
-							<input name="search" type="submit" value="Search" class="search-submit" />
-						</div>
-					</form>	
-      	</div>
-
-	
+	<div>
+		<a href="http://roshow.net/feed/" title="Syndicate this site using RSS 2.0" target="_blank"><img src="http://roshow.net/wp-content/uploads/2011/02/rss1.png"></a>
+		<a href="http://twitter.com/roshow" target="_blank"><img src="http://roshow.net/wp-content/uploads/2011/02/twitter.png"></a>
+		<a href="http://facebook.com/roshow" target="_blank"><img src="http://roshow.net/wp-content/uploads/2011/02/facebook.png"></a>
 	</div>
-
-
-</div>
-<div id="sidebar-bottom"></div>
+	<div style="position:relative;">
+		<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+		<script>
+			new TWTR.Widget({
+			  version: 2,
+			  type: 'profile',
+			  rpp: 5,
+			  interval: 6000,
+			  width: 'auto',
+			  height: 'auto',
+			  theme: {
+			    shell: {
+			      background: '#eeeab6',
+			      color: '#0F0F0F'
+			    },
+			    tweets: {
+			      background: '#eeeab6',
+			      color: '#000000',
+			      links: '#000000'
+			    }
+			  },
+			  features: {
+			    scrollbar: false,
+			    loop: false,
+			    live: false,
+			    hashtags: true,
+			    timestamp: true,
+			    avatars: false,
+			    behavior: 'all'
+			  }
+			}).render().setUser('roshow').start();
+		</script>
+	</div>
+	<div class="sidebar_content">
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : endif?>
+	</div>
 
 </div>
